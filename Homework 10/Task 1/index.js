f82ad69d959b638bdfb06c53312dca037d038a26
Mark <markiyan.chornovil@gmail.com>
2022-09-07 14:24:25 +0300

    corrected

--- Homework 10/Task 1/index.js
+++ Homework 10/Task 1/index.js
@@ -8,50 +8,46 @@ const form = document.getElementById("form");
 form.addEventListener("submit", function (event) {
     i += 1;
     event.preventDefault();
-    if (document.getElementById("task").value.length === 0) {
-        alert("You can't add empty field!");
-    } else {
-        let div = document.createElement("div");
-        div.setAttribute("class", "wrapper");
-        document.body.appendChild(div);
+    let div = document.createElement("div");
+    div.setAttribute("class", "wrapper");
+    document.body.appendChild(div);
 
-        text = this.elements[0].value;
-        const area = document.createElement("p");
-        area.innerText = text;
-        div.appendChild(area);
+    text = this.elements[0].value;
+    const area = document.createElement("p");
+    area.innerText = text;
+    div.appendChild(area);
 
-        const checkbox = document.createElement("input");
-        checkbox.setAttribute("type", "checkbox");
-        checkbox.setAttribute("name", "checkbox");
-        checkbox.setAttribute("class", "checkbox");
-        div.appendChild(checkbox);
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("name", "checkbox");
+    checkbox.setAttribute("class", "checkbox");
+    div.appendChild(checkbox);
 
-        const removeButton = document.createElement("button");
-        removeButton.setAttribute("id", `btnRemoveTask${i}`);
-        removeButton.setAttribute("class", "removeButton");
-        removeButton.innerText = "Remove";
-        div.appendChild(removeButton);
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("id", `btnRemoveTask${i}`);
+    removeButton.setAttribute("class", "removeButton");
+    removeButton.innerText = "Remove";
+    div.appendChild(removeButton);
 
-        const btnRemoveTask = document.getElementById(`btnRemoveTask${i}`);
-        btnRemoveTask.addEventListener("click", removeAll);
+    const btnRemoveTask = document.getElementById(`btnRemoveTask${i}`);
+    btnRemoveTask.addEventListener("click", removeAll);
 
-        checkbox.addEventListener("change", function () {
-            if (this.checked) {
-                area.setAttribute("class", "line-through ");
-                console.log("Checkbox is checked..");
-            } else {
-                area.setAttribute("class", "");
-                console.log("Checkbox is not checked..");
-            }
-        });
-
-        this.elements[0].value = "";
-
-        function removeAll() {
-            area.remove();
-            removeButton.remove();
-            checkbox.remove();
-            div.remove();
+    checkbox.addEventListener("change", function () {
+        if (this.checked) {
+            area.setAttribute("class", "line-through ");
+            console.log("Checkbox is checked..");
+        } else {
+            area.setAttribute("class", "");
+            console.log("Checkbox is not checked..");
         }
-     }
+    });
+
+    this.elements[0].value = "";
+
+    function removeAll() {
+        area.remove();
+        removeButton.remove();
+        checkbox.remove();
+        div.remove();
+    }
 });
--- Homework 11/Task 1/index.js
+++ Homework 11/Task 1/index.js
@@ -9,75 +9,70 @@ const form = document.getElementById("form");
 form.addEventListener("submit", function (event) {
     i += 1;
     event.preventDefault();
-    if (document.getElementById("task").value.length === 0) {
-        alert("You can't add empty field!");
-    } else {
+    const newTask = {
+        id: taskArray.length === 0 ? 0 : taskArray[taskArray.length - 1].id + 1,
+        name: form.elements[0].value,
+        isDone: false,
+    };
 
-        const newTask = {
-            id: taskArray.length === 0 ? 0 : taskArray[taskArray.length - 1].id + 1,
-            name: form.elements[0].value,
-            isDone: false,
-        };
+    taskArray.push(newTask);
 
-        taskArray.push(newTask);
+    console.log(taskArray);
 
-        console.log(taskArray);
+    const div = createNode("div", [
+        { name: "class", value: "wrapper" },
+        { name: "data-id", value: newTask.id },
+    ]);
+    document.body.appendChild(div);
 
-        const div = createNode("div", [
-            { name: "class", value: "wrapper" },
-            { name: "data-id", value: newTask.id },
-        ]);
-        document.body.appendChild(div);
+    text = this.elements[0].value;
 
-        text = this.elements[0].value;
+    const area = createNode("p", [{ name: "class", value: "default" }]);
+    area.innerText = text;
+    div.appendChild(area);
 
-        const area = createNode("p", [{ name: "class", value: "default" }]);
-        area.innerText = text;
-        div.appendChild(area);
+    const checkbox = createNode("input", [
+        { name: "type", value: "checkbox" },
+        { name: "name", value: "checkbox" },
+        { name: "class", value: "checkbox" },
+    ]);
+    div.appendChild(checkbox);
 
-        const checkbox = createNode("input", [
-            { name: "type", value: "checkbox" },
-            { name: "name", value: "checkbox" },
-            { name: "class", value: "checkbox" },
-        ]);
-        div.appendChild(checkbox);
+    const removeButton = createNode("button", [
+        { name: "id", value: `btnRemoveTask${i}` },
+        { name: "class", value: "removeButton" },
+    ]);
+    removeButton.innerText = "Remove";
+    div.appendChild(removeButton);
 
-        const removeButton = createNode("button", [
-            { name: "id", value: `btnRemoveTask${i}` },
-            { name: "class", value: "removeButton" },
-        ]);
-        removeButton.innerText = "Remove";
-        div.appendChild(removeButton);
+    const btnRemoveTask = document.getElementById(`btnRemoveTask${i}`);
+    btnRemoveTask.addEventListener("click", removeAll);
 
-        const btnRemoveTask = document.getElementById(`btnRemoveTask${i}`);
-        btnRemoveTask.addEventListener("click", removeAll);
+    checkbox.addEventListener("change", function () {
 
-        checkbox.addEventListener("change", function () {
+        const wrapper = this.closest(".wrapper");
+        const id = wrapper.getAttribute("data-id");
 
-            const wrapper = this.closest(".wrapper");
-            const id = wrapper.getAttribute("data-id");
+        const task = taskArray.find((taskItem) => taskItem.id == id);
+        task.isDone = this.checked;
 
-            const task = taskArray.find((taskItem) => taskItem.id == id);
-            task.isDone = this.checked;
-
-            if (task.isDone) {
-                area.setAttribute("class", "line-through ");
-                console.log("Checkbox is checked..");
-            } else {
-                area.setAttribute("class", "");
-                console.log("Checkbox is not checked..");
-            }
-        });
-
-        this.elements[0].value = "";
-
-        function removeAll() {
-            area.remove();
-            removeButton.remove();
-            checkbox.remove();
-            div.remove();
+        if (task.isDone) {
+            area.setAttribute("class", "line-through ");
+            console.log("Checkbox is checked..");
+        } else {
+            area.setAttribute("class", "");
+            console.log("Checkbox is not checked..");
         }
-     }
+    });
+
+    this.elements[0].value = "";
+
+    function removeAll() {
+        area.remove();
+        removeButton.remove();
+        checkbox.remove();
+        div.remove();
+    }
 });
 
 function createNode(tagName, attributes) {
